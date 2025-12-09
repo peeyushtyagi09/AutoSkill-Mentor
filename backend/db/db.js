5b7610a8b6aaf73b01aac779eb1cd71cceebd163
peeyushtyagi09 <peeyushtyagi82@gmail.com>
2025-12-09 14:22:54 +0530

    fixing soem code and index.js and db.js file

--- backend/db/db.js
+++ backend/db/db.js
@@ -3,6 +3,11 @@ const env = require("./../example.env");
 
 const connectDB = async () => {
     try {
-        
+        await mongoose.connect(env.MONGODB_URL)
+        console.log("MongoDB is connected successfully !! ")
+    } catch (err) {
+        console.error(`MongoDB connection error ${err}`);
     }
 }
+
+module.exports = { connectDB };
--- backend/index.js
+++ backend/index.js
@@ -1,7 +1,26 @@
 const express = require("express");
 const cors = require("cors");
-const helmet = require("mongoose");
+const helmet = require("helmet");
 const env = require('./example.env');
+const { connectDB } = require("./db/db");
 
 const app = express();
 
+
+app.use(express.json());
+app.use(cors());
+app.use(helmet());
+
+
+// connection mongoDb connection 
+connectDB();
+
+app.get("/", (req, res) => {
+    res.send('Hello World!');
+})
+
+const port = env.PORT;
+
+app.listen(port, () => {
+    console.log(`Server is running on port ${port}`);
+})
